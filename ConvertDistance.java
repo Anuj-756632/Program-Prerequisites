8f41bce39b8c1f0cde7c393dc7d09850c2c66807
Anuj-756632 <anujvishwakarma302004@gmail.com>
2025-01-08 01:28:16 +0530

    Day01_practiceQuestion

---
+++ ConvertDistance.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ConvertDistance{
+       public static void main(String []args){
+	          Scanner sc=new Scanner(System.in);
+	          double d=sc.nextInt();
+			  double miles=d*0.621371;
+			  System.out.println("Distance in miles"+miles);
+		}
+}
