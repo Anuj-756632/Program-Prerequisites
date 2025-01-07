18d3b37939c711766670ee5171e786b22db5f0ea
Anuj-756632 <anujvishwakarma302004@gmail.com>
2025-01-08 00:59:35 +0530

    Day01_practiceQuestion

---
+++ Perimeter.java
@@ -0,0 +1,8 @@
+public class Perimeter{
+       public static void main(String []args){
+	          int l=20;
+			  int w=30;
+			  int perimeter=2*(l+w);
+			  System.out.println("perimeter of Rectangle is: "+perimeter);
+		}
+}
--- Volume.java
+++ Volume.java
@@ -1,7 +1,7 @@
 public class Volume{
        public static void main(String []args){
-	          int r=5;
-			  int h=2;
+	          int r=15;
+			  int h=20;
 			  double pi=3.14;
 			  double volume=pi*(r*r)*h;
 			  System.out.println("Volume of cylinder is: "+volume);
