b5879bfd63ea61d29754b90909234160afac73d9
Anuj-756632 <anujvishwakarma302004@gmail.com>
2025-01-08 00:28:19 +0530

    Day01_practiceQuestion

---
+++ Volume.java
@@ -0,0 +1,9 @@
+public class Volume{
+       public static void main(String []args){
+	          int r=5;
+			  int h=2;
+			  double pi=3.14;
+			  double volume=pi*(r*r)*h;
+			  System.out.println("Volume of cylinder is: "+volume);
+		}
+}
