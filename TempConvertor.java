3d4544c29fa5539db0211d90810277da5bbd84f6
Anuj-756632 <anujvishwakarma302004@gmail.com>
2025-01-08 00:03:50 +0530

    Day01_practiceQuestion

---
+++ TempConvertor.java
@@ -0,0 +1,7 @@
+public class TempConvertor{
+       public static void main(String []args){
+	          float celsius=10;
+			  float fahrenheit=((celsius*9)/5)+32;
+			  System.out.println("Temperature in Fahrenheit is: "+fahrenheit);
+		}
+}
